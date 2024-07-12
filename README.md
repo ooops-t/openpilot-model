e7b809d0fff994f0782f61e4a7ce8ce4df2355e2
m0nk <hac.m0nk@gmail.com>
2024-07-12 15:31:24 +0800

    show the yuv channel
    Signed-off-by: m0nk <hac.m0nk@gmail.com>

--- README.md
+++ README.md
@@ -8,5 +8,68 @@ tar -xvf onnxruntime-linux-x64-gpu-1.18.1.tgz
 
 ## Build
 ``` bash
-clang++ main.cpp -Ionnxruntime-linux-x64-gpu-1.18.1/include -Lonnxruntime-linux-x64-gpu-1.18.1/lib -lonnxruntime -lopencv_core -lopencv_videoio -lopencv_highgui
+clang++ main.cpp supercombomodel.cpp -Ionnxruntime-linux-x64-gpu-1.18.1/include -Lonnxruntime-linux-x64-gpu-1.18.1/lib -lonnxruntime -lopencv_core -lopencv_videoio -lopencv_highgui -lopencv_imgproc -lopencv_imgcodecs
+```
+
+## Test video information
+
+``` json
+// ffprobe -v quiet -print_format json -show_format -show_streams  video.hevc
+{
+    "streams": [
+        {
+            "index": 0,
+            "codec_name": "hevc",
+            "codec_long_name": "H.265 / HEVC (High Efficiency Video Coding)",
+            "profile": "Main",
+            "codec_type": "video",
+            "codec_tag_string": "[0][0][0][0]",
+            "codec_tag": "0x0000",
+            "width": 1164,
+            "height": 874,
+            "coded_width": 1184,
+            "coded_height": 896,
+            "closed_captions": 0,
+            "film_grain": 0,
+            "has_b_frames": 0,
+            "pix_fmt": "yuv420p",
+            "level": 93,
+            "color_range": "tv",
+            "chroma_location": "left",
+            "refs": 1,
+            "r_frame_rate": "25/1",
+            "avg_frame_rate": "25/1",
+            "time_base": "1/1200000",
+            "extradata_size": 74,
+            "disposition": {
+                "default": 0,
+                "dub": 0,
+                "original": 0,
+                "comment": 0,
+                "lyrics": 0,
+                "karaoke": 0,
+                "forced": 0,
+                "hearing_impaired": 0,
+                "visual_impaired": 0,
+                "clean_effects": 0,
+                "attached_pic": 0,
+                "timed_thumbnails": 0,
+                "captions": 0,
+                "descriptions": 0,
+                "metadata": 0,
+                "dependent": 0,
+                "still_image": 0
+            }
+        }
+    ],
+    "format": {
+        "filename": "video.hevc",
+        "nb_streams": 1,
+        "nb_programs": 0,
+        "format_name": "hevc",
+        "format_long_name": "raw HEVC video",
+        "size": "37467994",
+        "probe_score": 51
+    }
+}
 ```
--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
+#include <chrono>
 #include <iostream>
+#include <thread>
 
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/videoio.hpp>
 
 #include "supercombomodel.h"
@@ -9,13 +12,36 @@
 const char *model_path = "supercombo.onnx";
 const char *video_path = "video.hevc";
 
+static std::string type2str(int type) {
+  std::string r;
+
+  uchar depth = type & CV_MAT_DEPTH_MASK;
+  uchar chans = 1 + (type >> CV_CN_SHIFT);
+
+  switch ( depth ) {
+    case CV_8U:  r = "8U"; break;
+    case CV_8S:  r = "8S"; break;
+    case CV_16U: r = "16U"; break;
+    case CV_16S: r = "16S"; break;
+    case CV_32S: r = "32S"; break;
+    case CV_32F: r = "32F"; break;
+    case CV_64F: r = "64F"; break;
+    default:     r = "User"; break;
+  }
+
+  r += "C";
+  r += (chans+'0');
+
+  return r;
+}
+
 
 int main(int argc, char *argv[])
 {
     SupercomboModel model(model_path);
-
+#if 1
     // read video
-    cv::VideoCapture cap = cv::VideoCapture(video_path);
+    cv::VideoCapture cap = cv::VideoCapture(video_path, cv::CAP_ANY);
     if (!cap.isOpened()) {
         std::cerr << "ERROR! Unable to open video file" << std::endl;
         return -1; 
@@ -29,8 +55,56 @@ int main(int argc, char *argv[])
             break;
         }
 
-        cv::imshow("Live", frame);
+        // reize the frame
+        cv::Mat nframe;
+        cv::resize(frame, nframe, cv::Size(512, 256));
+        // std::cout << "Resize rows: " << nframe.rows << " cols: " << nframe.cols << " Channel: " << nframe.channels() << std::endl;
+
+        // convert to YUV420
+        cv::Mat yuv;
+        cv::cvtColor(nframe, yuv, cv::COLOR_BGR2YUV);
+
+        // split YUV
+        std::vector<cv::Mat> channels;
+        cv::split(yuv, channels);
+    
+        cv::imshow("Live Y", channels[0]);
+        cv::imshow("Live U", channels[1]);
+        cv::imshow("Live V", channels[2]);
+
+        cv::Mat newframe = cv::Mat::zeros(cv::Size(512, 256), CV_8UC(6));
+
+        std::vector<cv::Mat> t;
+        cv::split(newframe, t);
+        
+        cv::resize(channels[1], t[4], cv::Size(256, 128));
+        cv::resize(channels[2], t[5], cv::Size(256, 128));
+        cv::imshow("Live x", t[4]);
+
+        // 20Hz = 1 / 20 = 0.050s = 50ms
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
     }
 
+    cap.release();
+#else
+    cv::Mat frame = cv::imread("preview.png");
+    std::cout << "Depth: " <<  frame.depth() << " type: " << type2str(frame.type()) << std::endl;;
+    std::cout << "Origin rows: " << frame.rows << " cols: " << frame.cols << " Channel: " << frame.channels() << std::endl;
+
+    cv::Mat yuv;
+    cv::cvtColor(frame, yuv, cv::COLOR_BGR2YUV);
+
+    // split YUV
+    std::vector<cv::Mat> channels;
+    cv::split(yuv, channels);
+    std::cout << "channels rows: " << channels[0].rows << " cols: " << channels[0].cols << " Channel: " << channels[0].channels() << std::endl;
+    cv::imshow("Live Y", channels[0]);
+    cv::imshow("Live U", channels[1]);
+    cv::imshow("Live V", channels[2]);
+
+    while(cv::waitKey(5) <= 0) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    }
+#endif
     return 0;
 }
